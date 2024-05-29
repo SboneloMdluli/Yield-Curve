c2de4b47508f3437a0ac7be798858b466b88f582
SboneloMdluli <mdlulisbonelo@gmail.com>
2024-05-29 21:32:57 +0200

    WIP : Not sorted assumption and irregular frequency

---
+++ src/exception/YieldCurveException.java
@@ -0,0 +1,7 @@
+package exception;
+
+public class YieldCurveException extends Exception {
+  public YieldCurveException(String message) {
+    super(message);
+  }
+}
--- src/service/YieldCurve.java
+++ src/service/YieldCurve.java
@@ -1,5 +1,7 @@
 package service;
 
+import exception.YieldCurveException;
+
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
@@ -9,77 +11,73 @@ import java.util.TreeMap;
 
 public class YieldCurve {
 
-  private final TreeMap<LocalDate, List<Double>> entries = new TreeMap<>();
+    private final TreeMap<LocalDate, List<Double>> entries = new TreeMap<>();
 
-  public void put(LocalDate key, List<Double> value) {
-    entries.put(key, value);
-  }
+    public void put(LocalDate key, List<Double> value) {
+        entries.put(key, value);
+    }
 
-  private double requestBeyondLastDate(LocalDate date,Level level){
-      switch (level){
-          case BID:
-              return  entries.get(date).getFirst();
-          case ASK:
-              return  entries.get(date).getLast();
-          default:
-              return 0.5*(entries.get(date).getLast()-entries.get(date).getFirst());
-      }
-  }
+    private double requestBeyondLastDate(LocalDate date,Level level){
+        return switch (level) {
+            case BID -> entries.get(date).getFirst();
+            case ASK -> entries.get(date).getLast();
+            default -> 0.5 * (entries.get(date).getLast() - entries.get(date).getFirst())+entries.get(date).getFirst();
+        };
+    }
 
-  public double getRate(LocalDate request, Level level) {
+    public double getRate(LocalDate request, Level level) throws YieldCurveException {
 
-      // early exit if request > last date in dataset
-      if(request.isAfter(entries.lastKey())){
-          request = entries.lastKey();
-          return requestBeyondLastDate(request,level);
-      }
-      Map.Entry<LocalDate, List<Double>> start = entries.lowerEntry(request);
-      Map.Entry<LocalDate, List<Double>> end = entries.higherEntry(request);
-
-    List<Double> startValues = start.getValue();
-    List<Double> endValues = end.getValue();
-
-    Double x =
-        Double.valueOf(ChronoUnit.DAYS.between(start.getKey(), request)); // point to interpolate
-    Double endDate =
-        Double.valueOf(ChronoUnit.DAYS.between(start.getKey(), end.getKey())); // end date
-
-    switch (level) {
-      case BID:
-        {
-          Double[] arrayP1 = {0.0, startValues.getFirst()}; // x,y point1
-          List<Double> point1 = Arrays.asList(arrayP1);
-
-          Double[] arrayP2 = {endDate, endValues.getFirst()}; // x,y point2
-          List<Double> point2 = Arrays.asList(arrayP2);
-
-          return interpolate(point1, point2, x);
+        if (request.isBefore(entries.firstKey())) {
+            throw new YieldCurveException("Request date is before the smallest date in dataset");
         }
-
-      case ASK:
-        {
-          Double[] arrayP1 = {0.0, startValues.getLast()}; // x,y point1
-          List<Double> point1 = Arrays.asList(arrayP1);
-
-          Double[] arrayP2 = {endDate, endValues.getLast()}; // x,y point2
-          List<Double> point2 = Arrays.asList(arrayP2);
-
-          return interpolate(point1, point2, x);
+        // early exit if request > last date in dataset
+        if(request.isAfter(entries.lastKey())){
+            request = entries.lastKey();
+            return requestBeyondLastDate(request,level);
         }
-      default:
-        {
-          Double[] arrayP1 = {0.0, 0.5 * (startValues.getLast() - startValues.getFirst())}; // x,y point1
-          List<Double> point1 = Arrays.asList(arrayP1);
+        Map.Entry<LocalDate, List<Double>> start = entries.lowerEntry(request);
+        Map.Entry<LocalDate, List<Double>> end = entries.higherEntry(request);
 
-          Double[] arrayP2 = {endDate, 0.5 * (startValues.getLast() - startValues.getFirst())}; // (ASK-bid)/2
-          List<Double> point2 = Arrays.asList(arrayP2);
+        List<Double> startValues = start.getValue();
+        List<Double> endValues = end.getValue();
 
-          return interpolate(point1, point2, x);
+        Double x =
+                Double.valueOf(ChronoUnit.DAYS.between(start.getKey(), request)); // point to interpolate
+        Double endDate =
+                Double.valueOf(ChronoUnit.DAYS.between(start.getKey(), end.getKey())); // end date
+
+        switch (level) {
+            case BID -> {
+                Double[] arrayP1 = {0.0, startValues.getFirst()}; // x,y point1
+                List<Double> point1 = Arrays.asList(arrayP1);
+
+                Double[] arrayP2 = {endDate, endValues.getFirst()}; // x,y point2
+                List<Double> point2 = Arrays.asList(arrayP2);
+
+                return interpolate(point1, point2, x);
+            }
+            case ASK -> {
+                Double[] arrayP1 = {0.0, startValues.getLast()}; // x,y point1
+                List<Double> point1 = Arrays.asList(arrayP1);
+
+                Double[] arrayP2 = {endDate, endValues.getLast()}; // x,y point2
+                List<Double> point2 = Arrays.asList(arrayP2);
+
+                return interpolate(point1, point2, x);
+            }
+            default -> {
+                Double[] arrayP1 = {0.0, 0.5 * (startValues.getLast() - startValues.getFirst())+startValues.getFirst()}; // x,y point1
+                List<Double> point1 = Arrays.asList(arrayP1);
+
+                Double[] arrayP2 = {endDate, 0.5 * (startValues.getLast() - startValues.getFirst())+endValues.getFirst()}; // (ASK-bid)/2
+                List<Double> point2 = Arrays.asList(arrayP2);
+
+                return interpolate(point1, point2, x);
+            }
         }
     }
-  }
 
-  private static double interpolate(List<Double> p0, List<Double> p1, double x) {
-    return p0.get(1) + (x / p1.get(0)) * (p1.get(1) - p0.get(1));
-  }
+    private static double interpolate(List<Double> p0, List<Double> p1, double x) {
+        return p0.get(1) + (x / p1.get(0)) * (p1.get(1) - p0.get(1));
+    }
 }
